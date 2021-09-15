3a3eb4b34874a7c32fcfd6f5e811e94d85070a9e
rubencougil <rubencougil@gmail.com>
2021-09-15 11:36:29 +0200

    autocomplete with unformatted json

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"syscall/js"
 )
 
 func main() {
 	js.Global().Set("pretty", pretty())
+	js.Global().Set("time", time())
 	<-make(chan bool)
 }
 
@@ -25,6 +28,35 @@ func pretty() js.Func {
 
 		return pretty
 	})
+}
+
+func time() js.Func {
+	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		go func() {
+			time := getTime()
+			jsDoc := js.Global().Get("document")
+			jsonOutputTextArea := jsDoc.Call("getElementById", "jsoninput")
+			jsonOutputTextArea.Set("value", time)
+		}()
+
+		return nil
+	})
+}
+
+func getTime() string {
+	fmt.Printf("getting time... \n")
+	res, err := http.Get("https://worldtimeapi.org/api/timezone/Europe/Madrid")
+	if err != nil {
+		fmt.Printf("unable to get time: %s\n", err)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+	if err != nil {
+		fmt.Printf("unable to read time from response: %s\n", err)
+	}
+
+	return string(body)
 }
 
 func prettyJson(input string) (string, error) {
